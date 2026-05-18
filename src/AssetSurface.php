63dc18f7a2ea8b9c4673b2071517ee1be214b54c
Timothy McClatchey <timothy@kansatech.com>
2026-05-17 20:06:26 -0500

    0.3 alpha

--- src/AssetSurface.php
+++ src/AssetSurface.php
@@ -5,8 +5,19 @@ declare(strict_types=1);
 namespace CommonPHP\Assets;
 
 use CommonPHP\HTTP\Contracts\HttpSurfaceInterface;
+use CommonPHP\HTTP\Request;
+use CommonPHP\HTTP\Response;
+use CommonPHP\HTTP\ResponseFactory;
 
 class AssetSurface implements HttpSurfaceInterface
 {
+    public function supports(Request $request): bool
+    {
+        return $request->path() === '/assets' || str_starts_with($request->path(), '/assets/');
+    }
 
+    public function handle(Request $request): Response
+    {
+        return (new ResponseFactory())->notFound('Asset not found.');
+    }
 }
